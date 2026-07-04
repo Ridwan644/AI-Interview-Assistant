bd034d58f49df0dbe33f4e8f402d71fa865ba9d6
Ridwan Galib <ridwan21.21r@gmail.com>
2026-07-04 10:45:59 -0500

    Finished phase 2, Finished the database connection, and created the learn.py and created the table for learn.py

---
+++ backend/app/db/__init__.py
@@ -0,0 +1,6 @@
+#I have to import the moddels (so Base knows about them) 
+#create_all(engine) has to be called (so the tables get built) 
+
+#imports your learn class into the models package's __init__ 
+#from app.models.learn import Learn 
+
--- backend/app/models/learn.py
+++ backend/app/models/learn.py
@@ -1,10 +1,22 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey 
 from sqlalchemy.sql import func 
-
+from sqlalchemy.orm import relationship 
 from app.db.database import Base 
+
+'''
+Using an orm(Object Relational Mapepr) 
+What is an ORM?
+An ORM lets you map python classes direclty to database tables and treat 
+individual database rows as regular python objects 
+'''
 
 class Learn(Base): 
     __tablename__ = "learn"
     
-    id = column(Integer, primary_key = True, index=True)
-    title = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    
+    title = Column(String(255), unique=True, index=True, nullable=False)
+    topic = Column(String(100), index=True, nullable=False)
+    content = Column(Text,nullable=False)
+    #timestamp 
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
